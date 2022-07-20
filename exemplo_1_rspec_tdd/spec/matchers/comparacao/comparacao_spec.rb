c1c05d9e7128c3ef3ac918513a00b3eed8d9c6ac
Danniel Cristie <dany14012@gmail.com>
2022-07-19 21:37:12 -0300

    Aula : matchers de igualdade e comparação.

---
+++ exemplo_1_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
@@ -0,0 +1,29 @@
+describe "Mathers de Comparação" do
+  context "Comparação" do
+    it "#equal - Testa se é o mesmo objeto" do
+      x = "ruby"
+      y = "ruby"
+      expect(x).not_to equal(y)
+      expect(x).to equal(x)
+    end
+
+    it "#be - Testa se é o mesmo objeto" do
+      x = "ruby"
+      y = "ruby"
+      expect(x).not_to be(y)
+      expect(x).to be(x)
+    end
+
+    it "#eql - Testa o valor/conteudo" do
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eql(y)
+    end
+
+    it "#eq - Testa o valor/conteudo" do
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eq(x)
+    end
+  end
+end
