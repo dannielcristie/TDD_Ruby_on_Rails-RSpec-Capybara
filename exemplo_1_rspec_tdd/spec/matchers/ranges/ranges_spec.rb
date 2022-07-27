6c31384fb4040cb048022bd66798efee9b8ef81b
Danniel Cristie <dany14012@gmail.com>
2022-07-27 17:41:05 -0300

    Aula: One-liner syntax(Descrição Implicita).

--- exemplo_1_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
+++ exemplo_1_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
@@ -3,7 +3,14 @@ describe (1..5), "Matcher de Ranges" do
     it "#cover" do
       expect(subject).to cover(2)
       expect(subject).to cover(2, 5)
-      expect(subject).not_to cover(-1,6)
+      expect(subject).not_to cover(-1, 6)
     end
+
+    #One-liner syntax (descrição implicitas)
+    it { is_expected.to cover(2) }
+    it { is_expected.to cover(2, 5) }
+    it { is_expected.not_to cover(0, 6) }
+
+    it { expect(subject).to cover(2) }
   end
 end
