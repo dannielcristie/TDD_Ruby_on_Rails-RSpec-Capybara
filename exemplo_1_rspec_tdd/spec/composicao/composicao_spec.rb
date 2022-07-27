4ed3dd836fca65086962a085ac5e62fd8882d168
Danniel Cristie <dany14012@gmail.com>
2022-07-27 18:13:02 -0300

    Aula: Matcher All para coleções.

--- exemplo_1_rspec_tdd/spec/composicao/composicao_spec.rb
+++ exemplo_1_rspec_tdd/spec/composicao/composicao_spec.rb
@@ -1,7 +1,7 @@
 describe "Ruby on Rails" do
   it { is_expected.to start_with("Ruby").and end_with("Rails") }
 
-  it { expect(frutas).to eq("banana").or eq("laranja").or eq("uvw") }
+  it { expect(frutas).to eq("banana").or eq("laranja").or eq("uva") }
 
   def frutas
     %w( banana uva laranja ).sample
