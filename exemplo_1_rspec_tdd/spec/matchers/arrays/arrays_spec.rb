68ca9b808e4edde95e1b8bb24428159e0cd71f8d
Danniel Cristie <dany14012@gmail.com>
2022-07-28 16:47:07 -0300

    Aula: negativando matchers.

--- exemplo_1_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_1_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,9 +1,14 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1, 2, 3]), "Matchers para array " do
   context "Matchers para Array" do
     it "#include" do
       expect(subject).to include(2)
       expect(subject).to include(2, 1)
     end
+
+    it { expect(subject).to exclude(4) }
+
     it "#contain_exactly" do
       expect(subject).to contain_exactly(2, 1, 3)
     end
