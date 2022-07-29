5a174617813e83cbf773770564fe11de41fe5484
Danniel Cristie <dany14012@gmail.com>
2022-07-29 11:52:46 -0300

    Aula: Method Stubs

--- exemplo_1_rspec_tdd/spec/stubs/stubs_spec.rb
+++ exemplo_1_rspec_tdd/spec/stubs/stubs_spec.rb
@@ -14,4 +14,36 @@ describe "Stub" do
 
     expect(course_finished).to be_truthy
   end
+
+  it "Argumentos Dinâmicos" do
+    student = Student.new
+
+    allow(student).to receive(:foo) do |arg|
+      if arg == :hello
+        "ola"
+      elsif arg == :hi
+        "Hi!!!"
+      end
+    end
+    expect(student.foo(:hello)).to eq("ola")
+    expect(student.foo(:hi)).to eq("Hi!!!")
+  end
+
+  it "Qualquer instacia de classe" do
+    student = Student.new
+    other_student = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+
+    expect(student.bar).to be_truthy
+    expect(other_student.bar).to be_truthy
+  end
+
+  it "Erros" do
+    student = Student.new
+
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+
+    expect { student.bar }.to raise_error(RuntimeError)
+  end
 end
