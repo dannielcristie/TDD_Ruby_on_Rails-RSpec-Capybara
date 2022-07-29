c996cdd2587b8b4ef5398f856eff3705c519fd73
Danniel Cristie <dany14012@gmail.com>
2022-07-29 17:03:36 -0300

    Aula : As null Object - double test.

--- exemplo_1_rspec_tdd/spec/test_doubles/user_spec.rb
+++ exemplo_1_rspec_tdd/spec/test_doubles/user_spec.rb
@@ -5,4 +5,12 @@ describe "Teste Double" do
     puts user.nome
     puts user.password
   end
+
+  it "as_null_object" do
+    user = double("User").as_null_object
+    allow(user).to receive_messages(nome: "Danniel", password: "secret")
+    puts user.nome
+    puts user.password
+    user.abc
+  end
 end
