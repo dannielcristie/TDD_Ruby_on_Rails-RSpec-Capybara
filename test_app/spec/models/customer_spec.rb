b769ca3406a5eed849dedd77c4d33b43a307e796
Danniel Cristie <dany14012@gmail.com>
2022-08-01 16:26:48 -0300

    Rspec Fixtures

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :all
+
+  it "Create a Customer" do
+    customer = customers(:danniel)
+
+    expect(customer.full_name).to eq("Sr. Danniel Cristie")
+  end
 end
+    
