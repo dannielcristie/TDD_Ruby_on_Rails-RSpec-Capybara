e5317610c68ce24f135be9f2a18e13291bc999d0
Danniel Cristie <dany14012@gmail.com>
2022-08-02 18:48:34 -0300

    Aula: Sobrescrevendo Atributos e Aliases FactoryBot

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Customer, type: :model do
-  it "#full_name" do
-    customer = create(:customer)
-    expect(customer.full_name).to start_with("Sr.")
+  it "#full_name - Sobrescrevendo Atributos" do
+    customer = create(:customer, name: "Danniel")
+    expect(customer.full_name).to eq("Sr. Danniel")
   end
+
+  it "#full_name- usando aliases" do
+    customer = create(:user) # ou create(:customer)
+    expect(customer.full_name).to start_with("Sr. ")
+  end
+
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
 end
