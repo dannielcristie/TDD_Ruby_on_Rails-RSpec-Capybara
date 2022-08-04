7ed36094fb3d029c4c6cfa2d099b3f09f4fd5ce2
Danniel Cristie <dany14012@gmail.com>
2022-08-04 17:15:22 -0300

    Aula: VCR - modos de gravação de cassettes do VCR

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,5 +1,6 @@
 describe "HTTParty" do
-  it "content_type", vcr: { cassette_name: "jsonplaceholder/posts", match_requests_on: [:body] } do
+  #   it "content_type", vcr: { cassette_name: "jsonplaceholder/posts", match_requests_on: [:body] } do
+  it "content_type", vcr: { cassette_name: "jsonplaceholder/posts", record: :new_episodes } do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(status: 200, body: "", headers: { 'Content-type': "application/json " })
 
