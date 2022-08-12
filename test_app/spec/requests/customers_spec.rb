e622d98248545b3597edde8d9a439912c3a89fc9
Danniel Cristie <dany14012@gmail.com>
2022-08-12 17:11:11 -0300

    Aula: Testando API com Rspec - GET
    - testando metodo show e index de um api

---
+++ test_app/spec/requests/customers_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Customers", type: :request do
+  describe "GET /customers" do
+    it "works! 200 OK" do
+      get customers_path
+      expect(response).to have_http_status(200)
+    end
+
+    it "index - JSON 200 OK" do
+      get "/customers.json"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json([
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String),
+      ])
+    end
+
+    it "show - JSON 200 OK" do
+      get "/customers/1.json"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String),
+      )
+    end
+  end
+end
+0
