fd8a056fc3b61ed8f41f23ec750b66c509179aee
Danniel Cristie <dany14012@gmail.com>
2022-08-14 18:37:42 -0300

    Test:  teste para mostrar um cliente por id

--- tdd_app/spec/features/customer_spec.rb
+++ tdd_app/spec/features/customer_spec.rb
@@ -35,9 +35,22 @@ feature "Customers", type: :feature do
 
   scenario "Não Cadastra um cliente invalido" do
     visit(new_customer_path)
-
     click_on("Criar Cliente")
-
     expect(page).to have_content("não pode ficar em branco")
+  end
+
+  scenario "Mostrar um cliente" do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ["S", "N"].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png",
+    )
+
+    visit(customer_path(customer.id))
+    expect(page).to have_content(customer.name)
+    expect(page).to have_content(customer.email)
+    expect(page).to have_content(customer.phone)
   end
 end
